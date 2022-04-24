89673a9709dd8e6434f0fc2bc41ee53862c37413
Antanas Grigaitis <100243851+antyg123@users.noreply.github.com>
2022-04-24 17:59:53 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -18,15 +18,56 @@
 
 
 
-var operator;
-var number;
-var decimal;
-var clear;
-let result = '';
-
-function solve();
-function display(val);{
-    document.getElementById('result').value += val;
-    return val;
+//Add a number to the result
+let result = document.getElementById('result');
+let result2 = "";
+//function to be executed upon the click
+function displayOne(){
+    result.innerText += "1";
 }
-function clear();
+function displayTwo(){
+    result.innerText += "2";
+}
+function displayThree(){
+    result.innerText += "3";
+}
+function displayFour(){
+    result.innerText += "4";
+}
+function displayFive(){
+    result.innerText += "5";
+}
+function displaySix(){
+    result.innerText += "6";
+}
+function displaySeven(){
+    result.innerText += "7";
+}
+function displayEight(){
+    result.innerText += "8";
+}
+function displayNine(){
+    result.innerText += "9";
+}
+function displayZero(){
+    result.innerText += "0";
+}
+function clearResults(){
+    result.innerText="";
+}
+function addition(){
+    Number(result.innerText);
+
+    Number(results.innerText)*Number(result2);
+}
+function subtraction(){
+
+}
+function multiply(){
+
+}
+function division(){
+
+}
+
+
